41d3907e4025e5365beb68093b5af059a9d56263
avanisubbiah <avanishsubbiah1@gmail.com>
2023-02-14 17:06:58 -0500

    added zsh config

---
+++ config/zsh/prompt.zsh
@@ -0,0 +1,15 @@
+##
+## Prompt
+##
+
+# Load starship
+zinit ice as'command' from'gh-r' \
+  atload'export STARSHIP_CONFIG=$XDG_CONFIG_HOME/starship/starship.toml; eval $(starship init zsh)' \
+  atclone'./starship init zsh > init.zsh; ./starship completions zsh > _starship' \
+  atpull'%atclone' src'init.zsh'
+zinit light starship/starship
+
+. /home/avani/Repos/zsh-shell-mommy/shell-mommy/shell-mommy.sh
+precmd() { if (( $? == 0 )); then; echo $(success); else; echo $(failure); fi }
+mommy
+# vim:ft=zsh
